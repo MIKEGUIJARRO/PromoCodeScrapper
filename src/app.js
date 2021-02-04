8bcbcb1d4075ee963bb65e5caf2692b00489557a
Mike Guijarro <a01612042@itesm.mx>
2021-02-04 12:57:35 -0600

    Implementation of waitForResponse() method and text retrieve from DOM elements

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 const EMAIL = process.env.EMAIL;
 const PASSWORD = process.env.PASSWORD;
 
+
 (async () => {
     const browser = await puppeteer.launch({
         headless: false,
@@ -26,12 +27,41 @@ const PASSWORD = process.env.PASSWORD;
     //Click into the promoCode Navbar
     await clickPromoCodeNav(page);
     //Scrap the items and promocodes
-    let i = 0;
-    while (i != 1000) {
-        i++;
+    let steps = 0;
+    let productCounter = 0;
+    while (steps != 1) {
+        steps++;
         await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-        console.log(i);
-        await page.waitForTimeout(3000);
+        const response = await page.waitForResponse(response => {
+            return response.status() === 200;
+        });
+        if (response.request().url().includes("https://affiliate-program.amazon.com/home/promohub/promocodes/")) {
+            const LOAD_BATCH = 10;
+            console.log(steps);
+            for (let i = 0; i < LOAD_BATCH; i++) {
+                productCounter++;
+                //We ensure the visibility of the element
+                const itemElement = await page.waitForSelector(`.promo-item-display:nth-child(${productCounter})`);
+                //Accesing and manipulating the DOM element
+                                
+                const linkElement = await itemElement.$("input.a-button-input");
+                const promoElement = await itemElement.$("div.promo-category");
+                const linkPromoElement = await itemElement.$("a.a-link-normal");
+                const dateElement = await itemElement.$("span.a-size-small");
+
+                const promoText = await page.evaluate(el => el.textContent, promoElement);
+                const linkText = await page.evaluate(el => el.textContent, linkPromoElement);
+                const dateText = await page.evaluate(el => el.textContent, dateElement);
+
+                const item = {
+                    promoText,
+                    linkText,
+                    dateText,
+                }
+                console.log(productCounter, item);
+            }
+
+        }
     }
     await page.waitForTimeout(6000);
 
@@ -54,7 +84,7 @@ const loginInfo = async (page) => {
         await emailInput.type(EMAIL);
         await passwordInput.type(PASSWORD);
         await signInBtn.click();
-        await page.waitForNavigation();
+        await page.waitForNavigation({ waitUntil: "networkidle2" });
     } catch (e) {
         console.log(e);
     }
@@ -66,7 +96,7 @@ const clickPromoCodeNav = async (page) => {
         await promotionsNavLi.click();
         const amazonPromoCodes = await page.$('a[title="Amazon Promo Codes"]');
         await amazonPromoCodes.click();
-        await page.waitForNavigation();
+        await page.waitForNavigation({ waitUntil: "networkidle2" });
     } catch (e) {
         console.log(e);
     }
